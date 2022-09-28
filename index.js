ae11316376d5d3ea96b94e0214633ba72b5ee245
Faith Makinde <faithmakinde0@gmail.com>
2022-09-28 16:05:06 +0100

    implementing the messages

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let modal = document.getElementById("modal");
 let removeModal = document.getElementById("modal-close-btn");
-
+let form = document.getElementById("consent-btn");
+let text = document.getElementById("modal-text")
 setTimeout(function(){
     modal.style.display = "inline";
 }, 1500)
@@ -8,4 +9,18 @@ setTimeout(function(){
 
 removeModal.addEventListener("click", function(){
     modal.style.display = "none";
+})
+
+form.addEventListener("submit", function(event){
+  event.preventDefault();
+    text.innerHTML =    
+     `<div class="modal-inner-loading">
+    <img src="loading.svg" class="loading">
+    <p id="upload-text">Uploading your data to the dark web...</p>
+    </div>` 
+   
+  setTimeout(function(){
+    document.getElementById(id="upload-text").textContent = "Making the sale..."
+  }, 1500)
+
 })
