ecccd4ba6ff62297a5f0140a56b97ed5c2e90737
Faith Makinde <faithmakinde0@gmail.com>
2022-09-28 16:22:49 +0100

    passing messages

--- index.js
+++ index.js
@@ -23,4 +23,13 @@ form.addEventListener("submit", function(event){
     document.getElementById(id="upload-text").textContent = "Making the sale..."
   }, 1500)
 
+  setTimeout(function(){
+    document.getElementById("modal-inner").innerHTML = `<h2>Thanks you sucker! </h2>
+    <p>We just sold the rights to your eternal soul.</p>
+    <div class="idiot-gif">
+        <img src="pirate.gif">
+    </div>`
+
+  }, 3000)
+
 })
