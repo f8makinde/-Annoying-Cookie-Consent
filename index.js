8a768678627c939c0529fc0459ca60c9dacc9968
Faith Makinde <faithmakinde0@gmail.com>
2022-09-28 15:27:38 +0100

    fixed the css

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let modal = document.getElementById("modal");
 let removeModal = document.getElementById("modal-close-btn");
+
 setTimeout(function(){
     modal.style.display = "inline";
 }, 1500)
+
 
 removeModal.addEventListener("click", function(){
     modal.style.display = "none";
